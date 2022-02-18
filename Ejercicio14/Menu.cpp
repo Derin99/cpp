7f500f3ad4f3f5f411ec654f8d8281074be5bac4
Derin99 <98788838+Derin99@users.noreply.github.com>
2022-02-17 20:34:38 -0600

    Add files via upload

---
+++ Ejercicio14/Menu.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int opcion = 0;
+
+    while (true) {
+        system ("cls");
+
+        cout << "       M E N U       " << endl;
+     
+        cout << endl;
+        cout << " #1: - Cafe y Granitas" << endl;
+        cout << endl;
+        cout << " #2: - Reposteria" << endl;
+        cout << endl;
+        cout << " #0: - Salir" << endl; 
+		cout << endl;
+		cout << endl;
+        cout << "INGRESAR UNA OPCION DEL MENU: ";
+        
+        cin  >> opcion;
+
+        if (opcion ==1) {
+            system ("cls");
+            cout << "ESTAS EN EL MENU DE CAFE Y GRANITAS" << endl;
+            system("pause");
+        }
+
+        if (opcion ==2) {
+            system ("cls");
+            cout << "ESTAS EN EL MENU DE REPOSTERIA" << endl;
+            system("pause");
+        }
+
+        if (opcion ==0) {
+            break;
+        }
+    }
+    cout << "SALISTE DEL SISTEMA";
+      
+    return 0;
+}
